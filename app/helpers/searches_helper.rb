76a97e654bbcd4b9f44a21864f9ace9928a00f92
Nepuni <nepuni04@gmail.com>
2017-02-20 13:04:32 +0530

    Dashboard advance search

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -4,6 +4,13 @@ module SearchesHelper
         risks = risks.where(project: query[:project]) unless query[:project].blank?
         risks = risks.where(probability: query[:probability]) unless query[:probability].blank?
         risks = risks.where(impact: query[:impact]) unless query[:impact].blank?
+        unless query[:mitigation_target_from].blank? && query[:mitigation_target_to].blank?
+            risks = risks.where(["target_date >= ? AND target_date <= ?", query[:mitigation_target_from], query[:mitigation_target_to]])
+        end
+        category_ids = query[:categories].reject { |c| c.empty? }
+        if category_ids.size > 0
+            risks = risks.joins(:categories).where(categories: {id: category_ids}).distinct 
+        end
         return risks
     end
 end
